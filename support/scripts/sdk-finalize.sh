d014cee6309a136f19fb380bd0517bf9529cd437
Lalith Panditharatne <Lalith@access-is.com>
2018-01-10 17:03:29 +0000

    added buildroot config to sdk
    Signed-off-by: Lalith Panditharatne <Lalith@access-is.com>

--- support/scripts/sdk-finalize.sh
+++ support/scripts/sdk-finalize.sh
@@ -5,10 +5,10 @@ export SYSROOT=\$HOME/sdk/arm-buildroot-linux-gnueabihf/sysroot
 export PATH=\$HOME/sdk/bin:$PATH
 export HOST=arm-buildroot-linux-
 EOF
+
+cat .config | gzip -9 > output/host/.BRconfig.gz
 echo "Creating sdk tar"
 sdk_version=`git rev-parse --short HEAD`
 tar -czvf output/images/sdk-$sdk_version.tar.gz -C output/host/ .
-
-
-
-
+echo "all done, sdk is at output/images/sdk-$sdk_version.tar.gz"
+rm output/host/.BRconfig.gz
