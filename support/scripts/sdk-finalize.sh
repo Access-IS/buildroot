bf8a1c29b021d8217338207adfafca427327a1a5
Lalith Panditharatne <Lalith@access-is.com>
2018-02-14 10:55:55 +0000

    changed tar to plase sdk inside sdk/ dir
    Signed-off-by: Lalith Panditharatne <Lalith@access-is.com>

--- support/scripts/sdk-finalize.sh
+++ support/scripts/sdk-finalize.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
-cat > output/host/env.sh <<EOF
+
+
+cp -a output/host output/sdk
+
+cat > output/sdk/env.sh <<EOF
 export SYSROOT=\$HOME/sdk/arm-buildroot-linux-gnueabihf/sysroot
 export PATH=\$HOME/sdk/bin:$PATH
 export HOST=arm-buildroot-linux-
 EOF
 
-cat .config | gzip -9 > output/host/.BRconfig.gz
+cat .config | gzip -9 > output/sdk/.BRconfig.gz
 sdk_version=`git rev-parse --short HEAD`
-tar -czf output/images/sdk-$sdk_version.tar.gz -C output/host/ .
+tar -czf output/images/sdk-$sdk_version.tar.gz -C output/ sdk
 echo "created sdk : output/images/sdk-$sdk_version.tar.gz"
-rm output/host/.BRconfig.gz
+rm -rf output/sdk
 exit 0
